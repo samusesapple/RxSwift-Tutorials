c661ca48beb3acde80a28a12f9e960640e5cbb44
samusesapple <gwansammy@gmail.com>
2023-06-18 15:53:42 +0900

    tutorial1 - second commit

--- RxSwift-Tutorial-1/RxSwift-Tutorial/Views/MainViewController.swift
+++ RxSwift-Tutorial-1/RxSwift-Tutorial/Views/MainViewController.swift
@@ -12,12 +12,23 @@ import UIKit
 class ViewController: UIViewController {
     var disposeBag = DisposeBag()
 
+    let idValid: BehaviorSubject<Bool> = BehaviorSubject(value: false)
+    let idString: BehaviorSubject<String> = BehaviorSubject(value: "")
+    
+    let passwordValid: BehaviorSubject<Bool> = BehaviorSubject(value: false)
+    let passwordString: BehaviorSubject<String> = BehaviorSubject(value: "")
+    
+    var matchStatus: Bool = false
+    
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        bindUI()
+        bindInput()
+        bindOutput()
     }
 
-    // MARK: - IBOutler
+    // MARK: - IBOutlet
 
     @IBOutlet var idField: UITextField!
     @IBOutlet var pwField: UITextField!
@@ -25,16 +36,62 @@ class ViewController: UIViewController {
     @IBOutlet var idValidView: UIView!
     @IBOutlet var pwValidView: UIView!
 
-    // MARK: - Bind UI
-
-    private func bindUI() {
-        // id input +--> check valid --> bullet
-        //          |
-        //          +--> button enable
-        //          |
-        // pw input +--> check valid --> bullet
+    // MARK: - Bind
+    
+    private func bindInput() {
+   // input : id 입력, pw 입력
+        idField.rx.text.orEmpty
+            .bind(to: idString) // idField의 text를 외부변수에 전달
+            .disposed(by: disposeBag)
+        
+        idString // idField의 text를 전달받은 외부변수
+            .map(checkEmailValid) // 해당 변수 값의 email valid 여부 체크 -> bool
+            .bind(to: idValid) // vaild 여부에 대한 결과를 외부 변수에 전달
+            .disposed(by: disposeBag)
+        
+        pwField.rx.text.orEmpty
+            .bind(to: passwordString) // pwField의 text를 외부변수에 전달
+            .disposed(by: disposeBag)
+        
+        passwordString // pwField의 text를 전달받은 외부변수
+            .map(checkPasswordValid) // 해당 변수 값의 password valid 여부 체크 -> bool
+            .bind(to: passwordValid) // vaild 여부에 대한 결과를 외부 변수에 전달
+            .disposed(by: disposeBag)
     }
-
+    
+    private func bindOutput() {
+        // bullets - id
+        idValid.subscribe(onNext: { valid in
+            self.changeValidViewStatus(self.idValidView,
+                                       status: valid)
+        })
+            .disposed(by: disposeBag)
+        
+        // bullets - password
+        passwordValid.subscribe(onNext: { valid in
+            self.changeValidViewStatus(self.pwValidView,
+                                       status: valid)
+        })
+            .disposed(by: disposeBag)
+        
+        // login Button
+        Observable.combineLatest(idValid, passwordValid, resultSelector: { $0 && $1 })
+            .subscribe { enabled in
+                if enabled {
+                    self.loginButton.backgroundColor = .systemBlue
+                } else {
+                    self.loginButton.backgroundColor = .lightGray
+                }
+            }
+            .disposed(by: disposeBag)
+        
+        // check ID & PW
+        Observable.combineLatest(idString, passwordString)
+            .map({ $0 == "id@gmail.com" && $1 == "password" })
+            .subscribe { self.matchStatus = $0 }
+            .disposed(by: disposeBag)
+    }
+    
     // MARK: - Logic
 
     private func checkEmailValid(_ email: String) -> Bool {
@@ -44,4 +101,17 @@ class ViewController: UIViewController {
     private func checkPasswordValid(_ password: String) -> Bool {
         return password.count > 5
     }
+    
+    private func changeValidViewStatus(_ view: UIView, status valid: Bool) {
+        if valid {
+            view.backgroundColor = .green
+        } else {
+            view.backgroundColor = .red
+        }
+    }
+    
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        return matchStatus
+    }
+
 }
