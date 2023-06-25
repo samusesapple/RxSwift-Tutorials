d1308849fd897284e4aabeef9be6dcc802792ffb
samusesapple <gwansammy@gmail.com>
2023-06-25 18:43:46 +0900

    configure UI for new tutorial

---
+++ RxSwift-Tutorial-5/RxSwift-Tutorial-5/Views/MainViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RxSwift-Tutorial-5
+//
+//  Created by Sam Sung on 2023/06/25.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    // MARK: - Components
+    
+
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
